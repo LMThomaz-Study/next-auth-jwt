d0d49d29345d615c844b1267dc26e6472195dd1f
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-05-20 06:41:30 -0300

    Realizando refresh do token

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -9,3 +9,40 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies['next-auth-jwt.token']}`,
   },
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        cookies = parseCookies();
+
+        const { 'next-auth-jwt.refreshToken': refreshTokenCookie } = cookies;
+
+        api
+          .post('refresh', {
+            refreshToken: refreshTokenCookie,
+          })
+          .then((response) => {
+            const { token, refreshToken } = response.data;
+
+            setCookie(undefined, 'next-auth-jwt.token', token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: '/',
+            });
+
+            setCookie(undefined, 'next-auth-jwt.refreshToken', refreshToken, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: '/',
+            });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+          });
+      } else {
+        // deslogar o usuário
+      }
+    }
+  },
+);
